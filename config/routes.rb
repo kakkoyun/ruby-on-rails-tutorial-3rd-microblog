83131195b55c2986e2ba67dfc625a845d3509fbb
Kemal Akkoyun <kakkoyun@gmail.com>
2014-12-14 22:32:26 +0200

    Partials and paths.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/about'
-  get 'static_pages/help'
+  get 'help' => 'static_pages#help'
+  get 'static_pages/about', as: :about
+  get 'static_pages/contact', as: :contact
   get '/hello', to: 'application#hello'
 
 
